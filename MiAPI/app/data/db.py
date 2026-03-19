649fee14ba5ae782052ee195094f97b6b5c806f9
RuthPina8 <124050978@upq.edu.mx>
2026-03-18 18:56:17 -0600

    Primer avance practica 9

---
+++ MiAPI/app/data/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+#definimos la url de la db
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://admin:123456@postgres:5432/DB_miapi"
+)
+
+#2. creamos el motor de conexion 
+engine = create_engine(DATABASE_URL)
+
+#3. creamos gestionador de sesiones
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
